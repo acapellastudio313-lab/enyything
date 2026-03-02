c257927ced0802929f9c499f5da141c59e85e328
acapellastudio313-lab <acapellastudio313@gmail.com>
2026-03-02 22:25:51 +0700

    Delete agen-perubahan-pa-prabumulih directory

--- agen-perubahan-pa-prabumulih/src/pages/Explore.tsx
+++
@@ -1,9 +0,0 @@
-import RightSidebar from '../components/RightSidebar';
-
-export default function Explore() {
-  return (
-    <div className="w-full min-h-screen bg-white">
-      <RightSidebar isMobile={true} />
-    </div>
-  );
-}
